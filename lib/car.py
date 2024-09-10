123b8060b76e2c51deb179fd5c58c496786e89a4
toel-kamanda34 <toelokemwa@gmail.com>
2024-09-10 21:31:01 +0300

    completed code along

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,9 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def go(self):
+        print("VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!")
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    
+mercedes = Car(10,4)
+mercedes.go()
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,17 @@
 class Vehicle:
-    pass
+
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        print("vrrrrrrrooom!")
+        return "vrrrrrrrooom!"
+
+    def fill_up_tank(self):
+        print("filling up!")
+        return "filling up!"
+    
+mercedes = Vehicle(10,4)
+mercedes.go()
+mercedes.fill_up_tank()
